1ae3621500ec8986e65e1924027838e80c8552da
Spectra <spectranoid123@gmail.com>
2018-07-31 22:24:35 +0300

    Moved methods for lesson to separate class

---
+++ lesson.py
@@ -0,0 +1,57 @@
+class Lesson:
+
+    def getLessonTime(self,lesson):
+        """
+        Getting time of the lesson
+        :param lesson:
+        :return: two strings with lesson start and end time
+        """
+        return lesson['time_start'], lesson['time_end']
+
+    def getLessonType(self, lesson):
+        """
+        Getting type of the lesson
+        :param lesson:
+        :return: type of the lesson
+        """
+        return lesson['typeObj']['name']
+
+    def getGroupsNumbers(self, lesson):
+        """
+        Getting number of the the group
+        :param lesson:
+        :return: number of the group(str)
+        """
+        groupsNum = len(lesson['groups'])
+        groups = [None] * groupsNum
+        for group in range(groupsNum):
+            groups[group] = lesson['groups'][group]['name']
+        return groups
+
+    def getLessonTeacherName(self, lesson):
+        """
+        Getting name of the lesson teacher
+        :param lesson:
+        :return: name of the teacher
+        """
+        try:
+          teacherName = lesson['teachers'][0]['full_name']
+        except:
+          teacherName = "Неизвестно"
+
+        return teacherName
+
+    def getLessonAddress(self,lesson):
+        """
+        Getting address of the lesson
+        :param lesson:
+        :return: address of the lesson
+        """
+        try:
+          Address = lesson['auditories'][0]['building']['name'] + ", ауд. " + lesson['auditories'][0]['name']
+        except:
+          Address = "Неизвестно"
+
+        return Address
+
+        return lesson['auditories'][0]['building']['name'] + ", ауд. " + lesson['auditories'][0]['name']
--- schedule.py
+++ schedule.py
@@ -1,18 +1,20 @@
 import requests
 import json
+from lesson import Lesson
 
-class Schedule:
+class Schedule(Lesson):
 
-    def __init__(self, url):
-        self._url = url
+    def __init__(self, date):
+        self._url = "http://ruz2.spbstu.ru/api/v1/ruz/scheduler/24118?date=" + date
 
     def getWeekSchedule(self):
         """
         Getting week schedule
         :return: list with day's schedule
         """
-        jsonData = json.loads(requests.get(self._url).text)
-        return list(jsonData.values())[1]
+        response = requests.get(self._url).text
+        jsonData = json.loads(response)
+        return jsonData['days']
 
     def getDaySchedule(self, day):
         """
@@ -23,60 +25,15 @@ class Schedule:
         return self.getWeekSchedule()[day]
 
 
-    def getLessonTime(self,lesson):
-        """
-        Getting time of the lesson
-        :param lesson:
-        :return: two strings with lesson start and end time
-        """
-        return lesson.get('time_start'), lesson.get('time_end')
 
-    def getLessonType(self, lesson):
-        """
-        Getting type of the lesson
-        :param lesson:
-        :return: type of the lesson
-        """
-        return lesson.get('typeObj').get('name')
-
-    def getGroupsNumbers(self, lesson):
-        """
-        Getting number of the the group
-        :param lesson:
-        :return: number of the group(str)
-        """
-        groupsNum = len(lesson.get('groups'))
-        groups = [None] * groupsNum
-        for group in range(groupsNum):
-            groups[group] = lesson.get('groups')[group].get('name')
-        return groups
-
-    def getLessonTeacherName(self, lesson):
-        """
-        Getting name of the lesson teacher
-        :param lesson:
-        :return: name of the teacher
-        """
-        return lesson.get('teachers')[0].get('full_name')
-
-    def getLessonAddress(self,lesson):
-        """
-        Getting address of the lesson
-        :param lesson:
-        :return: address of the lesson
-        """
-        return lesson.get('auditories')[0].get('building').get('name') + ", ауд. " + lesson.get('auditories')[0].get('name')
-
-
-
-url = "http://ruz2.spbstu.ru/api/v1/ruz/scheduler/24118?date=2018-5-14"
-s = Schedule(url)
+s = Schedule("2018-05-20")
 a = s.getDaySchedule(5)
 lessons = a.get('lessons')
 print(a.get('date'))
-print(lessons[0].get('subject'))
-print(s.getLessonType(lessons[0]))
-print(s.getLessonTime(lessons[0]))
-print(s.getLessonTeacherName(lessons[0]))
-print(s.getLessonAddress(lessons[0]))
+print(lessons[1].get('subject'))
+print(s.getLessonType(lessons[1]))
+print(s.getLessonTime(lessons[1]))
+print(s.getLessonTeacherName(lessons[2]))
+print(s.getGroupsNumbers(lessons[1]))
+print(s.getLessonAddress(lessons[1]))
 
