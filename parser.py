3b6adcf7e20acd8e1b0f8e14b5a6a75a50a9bf85
Spectra <spectranoid123@gmail.com>
2018-07-31 21:48:44 +0300

    Changed class structure

--- parser.py
+++ parser.py
@@ -6,85 +6,69 @@ class ScheduleParser:
     def __init__(self, url):
         self._url = url
 
-
+    def getWeekSchedule(self):
+        """
+        Getting week schedule
+        :return: list with day's schedule
+        """
+        return list(json.loads(requests.get(self._url).text).values())[1]
 
     def getDaySchedule(self, day):
         """
-        Getting schedule for a day
+        Getting day schedule
         :param day:
-        :return dictrionary with the day schedule:
+        :return: list with day information(weekday, date, lessons)
         """
-        return list(json.loads(requests.get(self._url).text).values())[1][day].get('lessons')
+        return self.getWeekSchedule()[day]
 
-    def getDayScheduleDate(self, day):
-        """
-        Getting date
-        :param day:
-        :return dictrionary with the date:
-        """
-        return list(self.decode().values())[1][day].get('date')
 
-    def getLessonDescription(self, day, lesson):
-        """
-        Getting information about the lesson
-        :param day , lesson:
-        :return dictrionary with  the lesson description:
-        """
-        return self.getDaySchedule(day)[lesson]
-
-    def getLessonName(self, day, lesson):
-        """
-        Getting name of the lesson
-        :param day:
-        :param lesson:
-        :return: return the lesson name
-        """
-        return self.getLessonDescription(day, lesson).get('subject')
-
-    def getLessonTime(self,day, lesson):
+    def getLessonTime(self,lesson):
         """
         Getting time of the lesson
         :param day:
         :param lesson:
         :return: two strings with lesson start and end time
         """
-        return self.getLessonDescription(day, lesson).get('time_start'), self.getLessonDescription(day, lesson).get('time_end')
+        return lesson.get('time_start'), lesson.get('time_end')
 
-    def getLessonType(self,day, lesson):
+    def getLessonType(self, lesson):
         """
         Getting type of the lesson
         :param day:
         :param lesson:
         :return: type of the lesson
         """
-        return self.getLessonDescription(day, lesson).get('typeObj').get('name')
+        return lesson.get('typeObj').get('name')
 
-    def getGroupNumber(self, day, lesson):
+    def getGroupsNumbers(self, lesson):
         """
         Getting number of the the group
         :param day:
         :param lesson:
         :return: number of the group(str)
         """
-        return self.getLessonDescription(day, lesson).get('groups')[0].get('name')
+        groups = ""
+        for group in range(len(lesson.get('groups'))):
+            groups = groups + " ," + lesson.get('groups')[group].get('name')
+        return groups
 
-    def getLessonTeacherName(self, day, lesson):
+    def getLessonTeacherName(self, lesson):
         """
         Getting name of the lesson teacher
         :param day:
         :param lesson:
         :return: name of the teacher
         """
-        return self.getLessonDescription(day, lesson).get('teachers')[0].get('full_name')
+        return lesson.get('teachers')[0].get('full_name')
 
-    def getLessonAddress(self, day, lesson):
+    def getLessonAddress(self,lesson):
         """
         Getting address of the lesson
         :param day:
         :param lesson:
         :return: address of the lesson
         """
-        return self.getLessonDescription(day, lesson).get('auditories')[0].get('building').get('name') + ", ауд. " + self.getLessonDescription(day, lesson).get('auditories')[0].get('name')
+        return lesson.get('auditories')[0].get('building').get('name') + ", ауд. " + lesson.get('auditories')[0].get('name')
 
 
 
@@ -96,5 +80,8 @@ class ScheduleParser:
 
 url = "http://ruz2.spbstu.ru/api/v1/ruz/scheduler/24118?date=2018-5-14"
 s = ScheduleParser(url)
-a = s.getLessonAddress(0,0)
+a = s.getDaySchedule(5)
+lessons = a.get('lessons')
+a = s.getLessonAddress(lessons[0])
 print(a)
+#  Получение лессонов
